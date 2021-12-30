bdf445b82b1e3a9682076b029227f46d889b09f9
Matt Hinchliffe <matt@maketea.co.uk>
2021-12-30 12:12:48 +0000

    Rename CSS class names to match new prop names

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -2,15 +2,6 @@ import React, { useContext } from 'react'
 import { GlobalContext } from '../contexts'
 import type { ClassNames } from '../sharedTypes'
 
-const LabelStyles: React.CSSProperties = {
-  position: 'absolute',
-  left: -10000,
-  top: 'auto',
-  width: 1,
-  height: 1,
-  overflow: 'hidden',
-}
-
 export type LabelProps = {
   ariaLabelText: string
   classNames: ClassNames
@@ -20,8 +11,8 @@ export function Label({ ariaLabelText, classNames }: LabelProps): JSX.Element {
   const { id } = useContext(GlobalContext)
 
   return (
-    <span className={classNames.label} id={`${id}-label`} style={LabelStyles}>
+    <div className={classNames.label} id={`${id}-label`}>
       {ariaLabelText}
-    </span>
+    </div>
   )
 }
