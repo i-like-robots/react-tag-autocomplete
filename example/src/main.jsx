2b4fa492aec9a36fe9122c1159a23889fd2f387f
Matt Hinchliffe <matt@maketea.co.uk>
2022-02-10 22:27:24 +0000

    Update examples

--- example/src/main.jsx
+++ example/src/main.jsx
@@ -4,16 +4,19 @@ import { ReactTags } from '../../src'
 import { suggestions } from './countries'
 
 /**
- * Demo 1 - Country selector
+ * Demo 1 - country selector with options
  */
 
 function CountrySelector() {
   const [selected, setSelected] = useState([suggestions[10], suggestions[121]])
-  const [isInvalid, setIsInvalid] = useState(false)
-  const [isDisabled, setIsDisabled] = useState(false)
-  const [closeOnSelect, setCloseOnSelect] = useState(false)
-  const [allowBackspace, setAllowBackspace] = useState(false)
-  const [allowTab, setAllowTab] = useState(false)
+
+  const [options, setOptions] = useState({
+    allowBackspace: false,
+    allowTab: false,
+    closeOnSelect: false,
+    isDisabled: false,
+    isInvalid: false,
+  })
 
   const onDelete = useCallback(
     (index) => {
@@ -29,49 +32,69 @@ function CountrySelector() {
     [selected]
   )
 
+  const onOptionChange = useCallback(
+    (e) => {
+      setOptions({ ...options, [e.target.name]: e.target.checked })
+    },
+    [options]
+  )
+
   return (
     <>
       <p>Select the countries you have visited below:</p>
       <ReactTags
-        allowBackspace={allowBackspace}
-        allowTab={allowTab}
-        closeOnSelect={closeOnSelect}
-        isDisabled={isDisabled}
-        isInvalid={isInvalid}
+        id="demo-1"
         labelText="Select countries"
-        selected={selected}
-        suggestions={suggestions}
         onDelete={onDelete}
         onAddition={onAddition}
-        noSuggestionsText="No matching countries"
+        selected={selected}
+        suggestions={suggestions}
+        {...options}
       />
       <fieldset>
         <legend>Options</legend>
         <label>
-          <input type="checkbox" checked={isDisabled} onChange={() => setIsDisabled(!isDisabled)} />
+          <input
+            type="checkbox"
+            name="isDisabled"
+            checked={options.isDisabled}
+            onChange={onOptionChange}
+          />
           Disable component
         </label>
         <label>
-          <input type="checkbox" checked={isInvalid} onChange={() => setIsInvalid(!isInvalid)} />
+          <input
+            type="checkbox"
+            name="isInvalid"
+            checked={options.isInvalid}
+            onChange={onOptionChange}
+          />
           Mark as invalid
         </label>
         <label>
           <input
             type="checkbox"
-            checked={allowBackspace}
-            onChange={() => setAllowBackspace(!allowBackspace)}
+            name="allowBackspace"
+            checked={options.allowBackspace}
+            onChange={onOptionChange}
           />
           Allow backspace key to delete selected tags
         </label>
         <label>
-          <input type="checkbox" checked={allowTab} onChange={() => setAllowTab(!allowTab)} />
+          <input
+            type="checkbox"
+            name="allowTab"
+            checked={options.allowTab}
+            onChange={onOptionChange}
+          />
           Allow tab key to trigger selection
         </label>
         <label>
           <input
             type="checkbox"
-            checked={closeOnSelect}
-            onChange={() => setCloseOnSelect(!closeOnSelect)}
+            name="closeOnSelect"
+            checked={options.closeOnSelect}
+            onChange={onOptionChange}
           />
           Close the listbox on select
         </label>
@@ -89,16 +112,15 @@ function CountrySelector() {
 ReactDOM.render(<CountrySelector />, document.getElementById('demo-1'))
 
 /**
- * Demo 2 - AllowCustom tags
+ * Demo 2 - custom tags
  */
 
 function isValid(value) {
-  return /^[a-z]{3,12}$/i.test(value)
+  return /^[a-z]{4,12}$/i.test(value)
 }
 
-function AllowCustomTags() {
+function CustomTags() {
   const [selected, setSelected] = useState([])
-  const [errorMessage, setErrorMessage] = useState(null)
 
   const onDelete = useCallback(
     (tagIndex) => {
@@ -109,41 +131,36 @@ function AllowCustomTags() {
 
   const onAddition = useCallback(
     (newTag) => {
-      if (isValid(newTag.label)) {
-        setSelected([...selected, newTag])
-        setErrorMessage('')
-      } else {
-        setErrorMessage('The tag is not valid')
-      }
+      setSelected([...selected, newTag])
     },
     [selected]
   )
+
+  const onValidate = useCallback((value) => isValid(value), [])
 
   return (
     <>
       <p>Enter new tags meeting the requirements below:</p>
       <ReactTags
         allowNew
-        closeOnSelect={true}
+        ariaDescribedBy="description-2"
+        closeOnSelect
+        id="demo-2"
         labelText="Enter new tags"
+        onAddition={onAddition}
+        onDelete={onDelete}
+        onValidate={onValidate}
         selected={selected}
         suggestions={[]}
-        onDelete={onDelete}
-        onAddition={onAddition}
       />
-      {errorMessage ? (
-        <p style={{ margin: '0.25rem 0', color: '#fd5956' }}>{errorMessage}</p>
-      ) : null}
-      <p style={{ margin: '0.25rem 0', color: 'gray' }}>
-        <small>
-          <em>Tags must be 3–12 characters in length and only contain the letters A-Z</em>
-        </small>
+      <p id="description-2" style={{ color: 'gray' }}>
+        <em>Tags must be between 4 and 12 characters in length and only contain the letters A-Z</em>
       </p>
     </>
   )
 }
 
-ReactDOM.render(<AllowCustomTags />, document.getElementById('demo-2'))
+ReactDOM.render(<CustomTags />, document.getElementById('demo-2'))
 
 /**
  * Demo 3 - custom validity
@@ -152,49 +169,48 @@ ReactDOM.render(<AllowCustomTags />, document.getElementById('demo-2'))
 function CustomValidity() {
   const [selected, setSelected] = useState([])
 
-  const isValid = selected.length === 3
-
   const onDelete = useCallback(
     (index) => {
       setSelected(selected.filter((_, i) => i !== index))
-      return true
     },
     [selected]
   )
 
   const onAddition = useCallback(
     (newTag) => {
-      if (!isValid) {
-        setSelected([...selected, newTag])
-        return true
-      }
-
-      return false
+      setSelected([...selected, newTag])
     },
-    [isValid, selected]
+    [selected]
   )
+
+  const length = 3
 
   return (
     <>
-      <p>
-        Please select <em>exactly</em> 3 tags:
+      <p id="description-3">
+        Please select <em>exactly</em> {length} tags:
       </p>
-      <div style={{ display: 'flex', gap: '0.75rem' }}>
-        <div style={{ flex: 1 }}>
-          <ReactTags
-            labelText="Select countries"
-            isInvalid={!isValid}
-            selected={selected}
-            suggestions={suggestions}
-            onDelete={onDelete}
-            onAddition={onAddition}
-            noSuggestionsText="No matching countries"
-          />
-        </div>
-        <div style={{ fontSize: '1.75rem' }} role="status">
-          {selected.length === 3 ? '🥳' : '😞'}
-        </div>
-      </div>
+      <ReactTags
+        ariaDescribedBy="description-3"
+        ariaErrorMessage="error"
+        id="demo-3"
+        isInvalid={selected.length !== length}
+        labelText="Select countries"
+        onDelete={onDelete}
+        onAddition={onAddition}
+        selected={selected}
+        suggestions={suggestions}
+      />
+      {selected.length < length ? (
+        <p id="error" style={{ color: '#fd5956' }}>
+          You must to select {length - selected.length} more tags
+        </p>
+      ) : null}
+      {selected.length > length ? (
+        <p id="error" style={{ color: '#fd5956' }}>
+          You must remove {selected.length - length} tags
+        </p>
+      ) : null}
     </>
   )
 }
@@ -205,8 +221,25 @@ ReactDOM.render(<CustomValidity />, document.getElementById('demo-3'))
  * Demo 4 - async suggestions
  */
 
-async function fetchData(query) {
+function debounce(fn, delay = 100) {
+  let timeoutID
+
+  return function (...args) {
+    clearTimeout(timeoutID)
+    timeoutID = setTimeout(() => fn(...args), delay)
+  }
+}
+
+function wait(delay = 100) {
+  return new Promise((resolve) => setTimeout(resolve, delay))
+}
+
+async function fetchData(value) {
   try {
+    const query = encodeURIComponent(value)
+
+    await wait()
+
     const response = await fetch(
       `https://api.openbrewerydb.org/breweries/autocomplete?query=${query}`
     )
@@ -224,64 +257,67 @@ async function fetchData(query) {
 }
 
 function AsyncSuggestions() {
-  const [isBusy, setIsBusy] = useState(false)
-  const [selected, setSelected] = useState([])
-  const [suggestions, setSuggestions] = useState([])
+  const [state, setState] = useState({
+    isBusy: false,
+    selected: [],
+    suggestions: [],
+  })
 
   const onDelete = useCallback(
     (index) => {
-      setSelected(selected.filter((_, i) => i !== index))
-      return true
+      setState({ ...state, selected: state.selected.filter((_, i) => i !== index) })
     },
-    [selected]
+    [state]
   )
 
   const onAddition = useCallback(
     (newTag) => {
-      setSelected([...selected, newTag])
-      setSuggestions([])
-      return true
+      setState({ ...state, selected: [...state.selected, newTag], suggestions: [] })
     },
-    [selected]
+    [state]
   )
 
-  const onInputChange = useCallback(
-    async (value) => {
-      if (!isBusy) {
-        setIsBusy(true)
+  const onInput = useCallback(
+    debounce(async (value) => {
+      if (state.isBusy) return
 
-        try {
-          const suggestions = await fetchData(value)
+      setState({ ...state, suggestions: [], isBusy: true })
 
-          if (suggestions) setSuggestions(suggestions)
-        } catch (error) {
-          alert('Oh no!')
+      try {
+        const suggestions = await fetchData(value)
+
+        if (suggestions) {
+          setState({ ...state, suggestions, isBusy: false })
         }
-
-        setIsBusy(false)
+      } catch (error) {
+        console.error(error)
+        setState({ isBusy: false })
       }
-    },
-    [isBusy]
+    }),
+    [state]
   )
 
-  const noSuggestionsText = isBusy ? 'Loading...' : 'No breweries found'
+  const noOptionsText =
+    state.isBusy && !state.suggestions.length ? 'Loading...' : 'No breweries found'
 
   return (
     <>
-      <p>
-        Select the breweries you have visited using React Tags below (powered by the{' '}
-        <a href="https://www.openbrewerydb.org/">Open Brewery DB</a>):
-      </p>
+      <p id="description-4">Select the breweries you have visited using React Tags below:</p>
       <ReactTags
+        ariaDescribedBy="description-4"
+        id="demo-4"
         labelText="Select breweries"
-        noSuggestionsText={noSuggestionsText}
-        onDelete={onDelete}
+        noOptionsText={noOptionsText}
         onAddition={onAddition}
-        onInput={onInputChange}
-        placeholderText="Start typing to fetch suggestions"
-        selected={selected}
-        suggestions={suggestions}
+        onDelete={onDelete}
+        onInput={onInput}
+        placeholderText="Start typing..."
+        selected={state.selected}
+        suggestions={state.suggestions}
       />
+      <p>
+        Demo powered by the <a href="https://www.openbrewerydb.org/">Open Brewery DB</a>
+      </p>
     </>
   )
 }
