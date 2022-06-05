bd1f4db48fa9b9823f0e55599833ed14c31460b9
Matt Hinchliffe <matt@maketea.co.uk>
2022-06-05 12:38:19 +0100

    Refactor tag component to use a render prop to enable custom renderers in future

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,19 +1,30 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../contexts'
 import { useSelectedTag } from '../hooks'
+import type { ClassNames } from '../sharedTypes'
 
-export type TagProps = {
-  title: string
-  index: number
+type TagComponentProps = React.ComponentPropsWithoutRef<'button'> & {
+  classNames: ClassNames
+  label: string
 }
 
-export function Tag({ index, title }: TagProps): JSX.Element {
-  const { classNames } = useContext(GlobalContext)
-  const { label, tagProps } = useSelectedTag(index, title)
-
+function TagComponent({ classNames, label, ...tagProps }: TagComponentProps): JSX.Element {
   return (
     <button type="button" className={classNames.tag} {...tagProps}>
       <span className={classNames.tagName}>{label}</span>
     </button>
   )
+}
+
+export type TagProps = {
+  render?: (props: TagComponentProps) => JSX.Element
+  title: string
+  index: number
+}
+
+export function Tag({ render = TagComponent, index, title }: TagProps): JSX.Element {
+  const { classNames } = useContext(GlobalContext)
+  const { label, tagProps } = useSelectedTag(index, title)
+
+  return render({ classNames, label, ...tagProps })
 }
