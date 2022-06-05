f5d77985c789a7beaa644c752f619ebbe28071b7
Matt Hinchliffe <matt@maketea.co.uk>
2022-06-05 18:11:22 +0100

    Refactor label component to use a render prop to enable custom renderers in future

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,17 +1,28 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../contexts'
 import { labelId } from '../lib'
+import type { ClassNames } from '../sharedTypes'
 
-export type LabelProps = {
+type LabelComponentProps = {
   children: React.ReactNode
+  classNames: ClassNames
+  id: string
 }
 
-export function Label({ children }: LabelProps): JSX.Element {
-  const { classNames, id } = useContext(GlobalContext)
-
+function LabelComponent({ children, classNames, id }: LabelComponentProps): JSX.Element {
   return (
-    <div className={classNames.label} id={labelId(id)}>
+    <div className={classNames.label} id={id}>
       {children}
     </div>
   )
+}
+
+export type LabelProps = {
+  children: React.ReactNode
+  render?: (props: LabelComponentProps) => JSX.Element
+}
+
+export function Label({ children, render = LabelComponent }: LabelProps): JSX.Element {
+  const { classNames, id } = useContext(GlobalContext)
+  return render({ children, classNames, id: labelId(id) })
 }
