120ca35ff37724b4b6534805f2cef521aa390e5e
Matt Hinchliffe <matt@maketea.co.uk>
2023-07-05 18:03:41 +0100

    Refactor example to run onload to avoid Safari bug, resolves #44

--- example/src/main.jsx
+++ example/src/main.jsx
@@ -6,17 +6,21 @@ import { CustomValidity } from './demos/CustomValidity'
 import { UsingTheAPI } from './demos/UsingTheAPI'
 import { AsyncSuggestions } from './demos/AsyncSuggestions'
 
-const container1 = ReactDOM.createRoot(document.getElementById('demo-1'))
-container1.render(<CountrySelector />)
+// HACK: Wait for onload to ensure styles are loaded due to a bug with Safari
+// <https://github.com/i-like-robots/react-tag-autocomplete/issues/44>
+window.onload = () => {
+  const container1 = ReactDOM.createRoot(document.getElementById('demo-1'))
+  container1.render(<CountrySelector />)
 
-const container2 = ReactDOM.createRoot(document.getElementById('demo-2'))
-container2.render(<CustomTags />)
+  const container2 = ReactDOM.createRoot(document.getElementById('demo-2'))
+  container2.render(<CustomTags />)
 
-const container3 = ReactDOM.createRoot(document.getElementById('demo-3'))
-container3.render(<CustomValidity />)
+  const container3 = ReactDOM.createRoot(document.getElementById('demo-3'))
+  container3.render(<CustomValidity />)
 
-const container4 = ReactDOM.createRoot(document.getElementById('demo-4'))
-container4.render(<AsyncSuggestions />)
+  const container4 = ReactDOM.createRoot(document.getElementById('demo-4'))
+  container4.render(<AsyncSuggestions />)
 
-const container5 = ReactDOM.createRoot(document.getElementById('demo-5'))
-container5.render(<UsingTheAPI />)
+  const container5 = ReactDOM.createRoot(document.getElementById('demo-5'))
+  container5.render(<UsingTheAPI />)
+}
